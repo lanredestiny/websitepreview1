fb92d10a469758ac7427bb9d7bd07919989b76ac
unknown <lanredestiny125@gmail.com>
2020-11-27 16:56:53 +0100

    master

---
+++ main.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  $('.menuIcon').click(function(){
+      $('body').toggleClass('menuShown');
+  });
+});
+
+$(document).ready(function(){
+  $("menu a").on('click', function(event) {
+
+    if (this.hash !== "") {
+
+      event.preventDefault();
+
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+var section = $('#blog li, .faq li');
+
+function toggleAccordion() {
+  section.removeClass('active');
+  $(this).addClass('active');
+}
+
+section.on('click', toggleAccordion);
